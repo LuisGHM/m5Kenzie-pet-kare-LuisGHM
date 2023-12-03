6fa35f3d0bdf62c6010876733b1878d48c97aca4
Luis <lgmarchioreh@gmail.com>
2023-12-03 11:04:52 -0300

    FEAT(pets) Added form for update a pet by ID

--- pets/views.py
+++ pets/views.py
@@ -42,22 +42,62 @@ class PetView(APIView, PageNumberPagination):
         serializer_pet = PetSerializer(result_page, many=True)
         
         return self.get_paginated_response(serializer_pet.data)
+    
 
 
 class petIdView(APIView):
     def get(self, request: Request, pet_id: int) -> Response:
         try:
             pet = Pet.objects.get(id=pet_id)
-        except:
+        except Pet.DoesNotExist:
             return Response({"detail": "Not found."}, status.HTTP_404_NOT_FOUND) 
         serializer_pet = PetSerializer(pet)
         return Response(serializer_pet.data)
     
     
+    def patch(self, request: Request, pet_id: int) -> Response:
+        try:
+            pet = Pet.objects.get(id=pet_id)
+        except Pet.DoesNotExist:
+            return Response({"detail": "Not found."}, status.HTTP_404_NOT_FOUND)
+
+        serializer_request = PetSerializer(pet, data=request.data, partial=True)
+        if not serializer_request.is_valid():
+            return Response(serializer_request.errors, status.HTTP_400_BAD_REQUEST)
+
+        group_data = serializer_request.validated_data.pop("group")
+        trait_data_list = serializer_request.validated_data.pop("traits")
+
+        new_traits = []
+        for trait_data in trait_data_list:
+            try:
+                trait = Trait.objects.get(name__iexact=trait_data["name"])
+                new_traits.append(trait)
+            except Trait.DoesNotExist:
+                new_trait = Trait.objects.create(**trait_data)
+                new_traits.append(new_trait)
+
+        try:
+            group = Group.objects.get(scientific_name__exact=group_data["scientific_name"])
+        except Group.DoesNotExist:
+            group = Group.objects.create(**group_data)
+
+        pet.group = group
+
+        pet.traits.set(new_traits)
+
+        for key, value in serializer_request.validated_data.items():
+            setattr(pet, key, value)
+
+        pet.save()
+
+        return Response(PetSerializer(pet).data)
+
+
     def delete(self, request: Request, pet_id: int) -> Response:
         try:
             pet = Pet.objects.get(id=pet_id)
-        except:
+        except Pet.DoesNotExist:
             return Response({"detail": "Not found."}, status.HTTP_404_NOT_FOUND)
         
         pet.delete()
